79fe5cf84dfe24b155ea028e9774ed45a2cebecd
harolddavies <harolddavies369@gmail.com>
2024-06-05 20:59:58 +0800

    print class info

--- pythonClass.py
+++ pythonClass.py
@@ -16,6 +16,12 @@ class Employee:
  
    def displayEmployee(self):
       print("Name : ", self.name,  ", Salary: ", self.salary)
-      
+
+print("Employee.__doc__:", Employee.__doc__)
+print("Employee.__name__:", Employee.__name__)
+print("Employee.__module__:", Employee.__module__)
+print("Employee.__bases__:", Employee.__bases__)
+print("Employee.__dict__:", Employee.__dict__)      
+
 employee = Employee('wrench',100000)
 employee.displayEmployee()
